0900981e3495a5ab34cbf3260a58c615a6e74085
562066559@qq.com <5922644houxi>
2019-03-30 17:25:51 +0800

    add

---
+++ config-center/src/main/java/cn/ce/st/config/ConfigApplication.java
@@ -0,0 +1,21 @@
+package cn.ce.st.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+*
+* @author hx
+* @date 2018/12/22
+*/
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
